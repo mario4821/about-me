1d84de57c2efa7d3d00bacb86ad24ede0e1a4d17
Mario Flores Jr <m.flores4821@gmail.com>
2018-01-14 19:56:54 -0800

    added CSS

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
-//counter to tally correct answers
+
 var tally = 0, counter = 0, flag = true, userNameInput;
-//Greeting
+
 function helloName () {
   userNameInput = prompt('What is your name?');
   console.log('The user entered:' + userNameInput);
@@ -9,28 +9,30 @@ function helloName () {
   alert('Hi, ' + userNameInput + '! Let\'s play a guessing game of random facts about me!');
 }
 helloName();
-//Question 1
-function cheesePizza() {
-  //do {
-  var favFood = prompt('Am I a huge fan of cheese pizza?').toUpperCase();
-  console.log('The user entered for cheese pizza:' + favFood);
 
-  if (favFood === 'No' || favFood === 'N') {
-    tally++;
-    flag === true;
-    alert('Correct! I need flavor!');
-  }
-  else if (favFood === 'YES' || favFood === 'Y') {
-    flag === true;
-    alert('Nope! Boring!');
-  } else {
-    flag === false;
-    alert('Please respond with Y, Yes, N, or No');
-  }
-} //while (flag === false);
-//}
+function cheesePizza() {
+  do {
+    var favFood = prompt('Am I a huge fan of cheese pizza?').toUpperCase();
+    console.log('The user entered for cheese pizza:' + favFood);
+
+    if (favFood === 'No' || favFood === 'N') {
+      tally++;
+      flag === false;
+      alert('Correct! I need flavor!');
+      break;
+    }
+    else if (favFood === 'YES' || favFood === 'Y') {
+      flag === false;
+      alert('Nope! Boring!');
+      break;
+    } else {
+      flag === true;
+      alert('Please respond with Y, Yes, N, or No');
+    }
+  } while (flag === true);
+}
 cheesePizza();
-//Question 2
+
 function petOwner() {
   do {
     var pets = prompt('Do I have any pets?').toUpperCase();
@@ -38,40 +40,44 @@ function petOwner() {
 
     if (pets === 'NO' || pets === 'N') {
       tally++;
-      flag === true;
+      flag === false;
       alert('Correct! Although, I do want a dog at some point once I get it together.');
+      break;
     }
     else if (pets === 'YES' || pets === 'Y') {
-      flag === true;
-      alert('Not a pet owner at the moment. I do want a dog at some point once I get it together.');
-    } else {
       flag === false;
+      alert('Not a pet owner at the moment. I do want a dog at some point once I get it together.');
+      break;
+    } else {
+      flag === true;
       alert('Please respond with Y, Yes, N, or No');
     }
-  } while (flag === false);
+  } while (flag === true);
 }
 petOwner();
-// Question 3
+
 function crossCountry() {
   do {
     var roadTrip = prompt('Have I ever driven cross-country across the USA?').toUpperCase(); console.log('The user entered for cross-country roadtrip:' + roadTrip);
 
     if (roadTrip === 'YES' || roadTrip === 'Y') {
       tally++;
-      flag === true;
+      flag === false;
       alert('Correct! I drove cross country when I moved here from DC and I highly recommend it to anyone that gets the chance!');
+      break;
     }
     else if (roadTrip === 'NO' || roadTrip === 'N') {
-      flag = true;
+      flag = false;
       alert('Wrong! I drove cross country when I moved here from DC and I highly recommend it to anyone that gets the chance!');
+      break;
     } else {
-      flag === false;
+      flag === true;
       alert('Please respond with Y, Yes, N, or No');
     }
-  } while (flag === false);
+  } while (flag === true);
 }
 crossCountry();
-// Question 4
+
 function codeBefore(){
   do {
     var priorCode = prompt('Have I ever coded prior to Codefellows?').toUpperCase();
@@ -79,20 +85,22 @@ function codeBefore(){
 
     if (priorCode === 'NO' || priorCode === 'N') {
       tally++;
-      flag === true;
+      flag === false;
       alert('Correct! This is the begining of my coding journey.');
+      break;
     }
     else if (priorCode === 'YES' || priorCode === 'Y') {
-      flag === true;
-      alert('Nope. I had never coded prior to codefellows.');
-    } else {
       flag === false;
+      alert('Nope. I had never coded prior to codefellows.');
+      break;
+    } else {
+      flag === true;
       alert('Please respond with Y, Yes, N, or No');
     }
-  } while (flag === false);
+  } while (flag === true);
 }
 codeBefore();
-// Question 5
+
 function busRide() {
   do {
     var publicTrans = prompt('Do I own a car?').toUpperCase();
@@ -100,46 +108,47 @@ function busRide() {
 
     if (publicTrans === 'NO' || publicTrans === 'N') {
       tally++;
-      flag === true;
+      flag === false;
       alert('Correct! Public transportation for me!');
+      break;
     }
     else if (publicTrans === 'YES' || publicTrans === 'Y') {
-      flag === true;
-      alert('Nope! Public transporation for me!');
-    } else {
       flag === false;
+      alert('Nope! Public transporation for me!');
+      break;
+    } else {
+      flag === true;
       alert('Please respond with Y, Yes, N, or No');
     }
-  } while (flag === false);
+  } while (flag === true);
 }
 busRide();
 //Question 6 Months lived in Seattle
 function seattleFreeze(){
   var monthsHere = 14;
-  //var counter = 0;
-
   do{
     var monthsGuess = parseInt(prompt('How many months have i lived in Seattle so far?'));
     counter++;
     console.log('The user entered for months living in Seattle:' + counter.length);
     if(monthsGuess === monthsHere){
       tally++;
-      flag = true;
-      alert('Correct!! Been here a year and change.');
-    } else if(monthsGuess < 14){
       flag = false;
+      alert('Correct!! Been here a year and change.');
+      break;
+    } else if(monthsGuess < 14){
+      flag = true;
       alert('Nope! Been here longer!');
     } else {
-      flag = false;
+      flag = true;
       alert('Haven\'t been here that long!');
     } if (counter >= 5 && flag === false) {
       alert('Sorry! You maxed out your guesses!');
       break;
     }
-  } while(flag === false && counter <= 5);
+  } while(flag === true && counter <= 5);
 }
 seattleFreeze();
-//Question 7 States Lived In
+
 function statesHome() {
   var statesLived = ['TEXAS', 'VIRGINIA', 'D.C.'];
 
@@ -150,18 +159,20 @@ function statesHome() {
       if(difState === statesLived[i]){
         alert('Correct! I have lived in ' + statesLived.join(' '));
         tally++;
-        flag = true;
+        flag = false;
         console.log('The user guessed correct state lived' + statesLived);
         break;
       }
-    } if (flag === false);{
-      alert('Nope! Guess again!');
-    } if (counter > 5) {
-      alert('Sorry! You maxed out your guesses!');
-      flag === true;
-      //break;
+      else {
+        flag = true;
+        alert('Nope! Guess again!');
+      }
+      if (counter > 5) {
+        alert('Sorry! You maxed out your guesses!');
+        flag === false;
+      }
     }
-  } while(counter <= 5 && flag === false);
+  } while(counter <= 5 && flag === true);
 }
 statesHome();
 
